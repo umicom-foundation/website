cbe11c2e1fadb3675631eb22532fa7a8b71affb2
Sammy Hegab <sammy.hegab@outlook.com>
2025-02-21 03:04:17 +0000

    Added video section, sponsor images and some minor modifications

---
+++ templates/sections/video_section.php
@@ -0,0 +1,56 @@
+<section id="videos" class="foundation-videos">
+        <div class="container">
+            <div class="section-title">
+                <h3>Foundation Videos</h3>
+            </div>
+            <div class="row">
+   <!--         <div class="col-md-4">
+                    <div class="ratio ratio-16x9 mb-4">
+                        <iframe src="https://www.youtube.com/embed/_YJ9iwfIuBA?si=vh_FJzF3kMnXvWyC" title="Foundation Video 1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div> -->
+                <div class="col-md-12">
+                    <a data-fancybox="gallery" href="/videos/highlights.mp4" class="d-block mb-4">
+                        <video width="100%" controls>
+                            <source src="/videos/highlights.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a data-fancybox="gallery" href="/videos/gaza_cash_distribution_video.mp4" class="d-block mb-4">
+                        <video width="100%" controls>
+                            <source src="/videos/gaza_cash_distribution_video.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a data-fancybox="gallery" href="/videos/north_gaza_water_project.mp4" class="d-block mb-4">
+                        <video width="100%" controls>
+                            <source src="/videos/north_gaza_water_project.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a data-fancybox="gallery" href="/videos/gaza_cash_distribution_video.mp4" class="d-block mb-4">
+                        <video width="100%" controls>
+                            <source src="/videos/gaza_cash_distribution_video.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a data-fancybox="gallery" href="/videos/gaza_cash_distribution_video.mp4" class="d-block mb-4">
+                        <video width="100%" controls>
+                            <source src="/videos/gaza_cash_distribution_video.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </a>
+                </div>
+
+        </div>
+
+                        </div>
+    </section>
