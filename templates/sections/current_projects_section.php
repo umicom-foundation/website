1a778be072494790d5634d82a600130dd832fc89
Sammy Hegab <sammy.hegab@outlook.com>
2025-02-22 01:11:22 +0000

    Added more images and updated the site with sponsor and project images

--- templates/sections/current_projects_section.php
+++ templates/sections/current_projects_section.php
@@ -44,46 +44,95 @@
     <section id="projects" class="our-current-projects">
     <div class="container">
     <div class="section-title">
-                <h3>Our Current Projects</h3>
+                <h3>Current Projects</h3>
             </div>
             <div class="row">
             <div class="col-md-4">
                     <div class="project-card">
                         <img src="./images/projects/dates_project_2.jpg" alt="Project 1">
-                        <h4>Humanitarian Aid to Patients</h4>
+                        <h4>Dates For Gaza Project</h4>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="project-card">
-                        <img src="./images/projects/dates_project_2.jpg" alt="Project 2">
+                        <img src="./images/projects/project_551/foodpack_project_551.jpg" alt="Project 1">
+                        <h4>Gaza Foodpack Project</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/hand_sanitizer_551_551.jpg" alt="Project 1">
+                        <h4>Hand Sanitizer Project</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/sammy_image_551.jpg" alt="Pray for Gaza Project">
+                        <h4>Pray for Gaza Project</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/stand_with_palestine_project_551.jpg" alt="Stand with Palestine">
+                        <h4>Stand with Palestine</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/tent_housing_project_551_551.jpg" alt="Project 1">
+                        <h4>Building Homes</h4>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/warehouse_project_551.jpg" alt="Project 1">
+                        <h4>Food Distribution Center Project</h4>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/egypt_hospital_visit_551.jpg" alt="Project 1">
+                        <h4>Hospital Visit</h4>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="project-card">
+                        <img src="./images/projects/project_551/woman_cash_distribution_project_551_551.jpg" alt="Project 2">
                         <h4>Financial Assistance in Gaza</h4>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div class="project-card">
-                        <img src="./images/projects/dates_project_2.jpg" alt="Project 3">
-                        <h4>Building Free Homes</h4>
+                        <img src="./images/projects/project_551/clothes_distribution_551.jpg" alt="Project 3">
+                        <h4>Clothes Distribution Project</h4>
                     </div>
                 </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                     <div class="project-card">
                         <h4>Educational Resources</h4>
-                        <img src="./images/projects/dates_project_2.jpg" alt="Project 1">
-                        <p>Providing resources for students of all ages</p>
+                        <img src="./images/projects/project_551/free_education_551.jpg" alt="Project 1">
+                       <!-- <p>Providing resources for students of all ages</p> -->
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div class="project-card">
-                        <img src="./images/projects/dates_project_2.jpg" alt="Project 2">
+                        <img src="./images/projects/project_551/north_gaza_water_project_551.jpg" alt="Project 2">
                         <h4>Clean Water Project</h4>
-                        <p>Clean and safe drinking water Distribution.</p>
+                     <!--   <p>Clean and safe drinking water Distribution.</p> -->
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div class="project-card">
-                     <img src="./images/projects/dates_project_2.jpg" alt="Project 3">
-                        <h4>Free software Development</h4>
-                        <p>Open Source Development</p>
+                     <img src="./images/projects/project_551/solarpanel_project_551.jpg" alt="Solar Power Project">
+                        <h4>Solar Power Project</h4>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="project-card">
+                     <img src="./images/projects/project_551/opensource_551.jpg" alt="Opensource Projects">
+                        <h4>OpenSource Development</h4>
                     </div>
                 </div>
                 </div>
--- templates/sections/sponsor_section.php
+++ templates/sections/sponsor_section.php
@@ -2,23 +2,35 @@
         <h2 class="section-heading">Our Sponsors & Strategic Partners</h2>
         <div class="container partners">
             <div class="row">
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/easternbridge.png" alt="Eastern Bridge">
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="80%" src="/images/partners/logo/easternbridge_851_261.jpg" alt="Eastern Bridge">
                 </div>
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/apacuk.svg" alt="Association of Palestinian Community UK">
                 </div>
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/bcde_logo.svg" alt="bcde">
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="70%" src="/images/partners/logo/apcuk_logo_434_261.jpg" alt="Association of Palestinian Community UK">
                 </div>
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/umicom_logo.svg" alt="bcde">
                 </div>
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/easternbridge.png" alt="Eastern Bridge">
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="70%" src="/images/partners/logo/bcde_logo_337_261.jpg" alt="bcde">
                 </div>
-                <div class="col-md-4 mb-4">
-                    <img width="50%" src="/images/partners/umicom_logo.svg" alt="bcde">
+                </div>
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="70%" src="/images/partners/logo/NMCUBE_271_261.jpg" alt="NM CUBE">
+                </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="70%" src="/images/partners/logo/youth_star_center_logo_261.jpg" alt="Youth Stars Center">
+                </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                <div class="image-container">
+                    <img width="70%" src="/images/partners/logo/umicom_logo.svg" alt="umicom">
+                </div>
                 </div>
             </div>
         </div>
